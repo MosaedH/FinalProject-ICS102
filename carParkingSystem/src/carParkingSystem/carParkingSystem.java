b19ac47b2ae5c20cd5e91a1ad075a0f770de1fd2
MosaedH <75680991+MosaedH@users.noreply.github.com>
2022-03-27 20:50:20 +0300

    ArrayList Change
    ArrayList Change

--- carParkingSystem/src/carParkingSystem/carParkingSystem.java
+++ carParkingSystem/src/carParkingSystem/carParkingSystem.java
@@ -77,9 +77,9 @@ public class carParkingSystem {
 			}
 		
 		// Start ----- ArrayList
-		ArrayList<String> timesF = new ArrayList<>(Arrays.asList("10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
-		ArrayList<String> timesS = new ArrayList<>(Arrays.asList("10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
-		ArrayList<String> timesT = new ArrayList<>(Arrays.asList("10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+		ArrayList<String> timesF = new ArrayList<>(Arrays.asList("1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"));
+		ArrayList<String> timesS = new ArrayList<>(Arrays.asList("1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"));
+		ArrayList<String> timesT = new ArrayList<>(Arrays.asList("1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"));
 		ArrayList<String> timesThatRemoved = new ArrayList<>(Arrays.asList());
 		// End ----- ArrayList
 		
